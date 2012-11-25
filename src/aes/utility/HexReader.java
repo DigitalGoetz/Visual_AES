42422a4643d015b44f8f963ea8b211bc6899c8c5
Michael Goetz <digital.goetz@gmail.com>
2012-11-25 15:49:20 -0500

    pre-test utils

--- src/aes/utility/HexReader.java
+++ src/aes/utility/HexReader.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aes.utility;
 
-/**
- *
- * @author michael
- */
 public class HexReader {
     
     String hexString;
